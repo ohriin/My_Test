da688988df7f745e4b652ee51efa14883b35d468
Roian Lim <ohriin@gmail.com>
2012-02-05 14:44:31 +0900

    My First Commit for my StudyJava

---
+++ StudyJava/src/com/home/MyRenderer.java
@@ -0,0 +1,12 @@
+package com.home;
+
+import javax.swing.JMenu;
+
+public class MyRenderer extends JMenu{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ StudyJava/src/com/home/StudyMain.java
@@ -0,0 +1,63 @@
+package com.home;
+
+import java.awt.Shape;
+import java.util.ArrayList;
+
+public class StudyMain {
+
+	public static void main(String[] args) {
+		
+		
+		String a = new String("ddd");
+		String b = new String("ddd");
+		
+		System.out.println(a.equals(b));
+		System.out.println(a == b);
+		
+//		ArrayList<String> array = new ArrayList<String>();
+//		Vector vector = new Vector();
+//		ArrayList<Area> arrayInteger = new ArrayList<Area>();
+//		
+//		array.add("a");
+//		array.add("b");
+//		array.add("c");
+//		
+//		vector.add("a");
+//		vector.add("b");
+//		vector.add("c");
+//		
+//		for(Object value : array) {
+//			String string = (String)value;
+//			System.out.println(string);
+//		}
+//		
+//		System.out.println("    ");
+//		
+//		for(Object value : vector) {
+//			String string = (String)value;
+//			System.out.println(string);
+//		}
+//		
+//		addTest(arrayInteger);
+////		ArrayList<? extends Number> ss = getTest();
+////		Integer a = ss.get(0);
+////		ss.add(new Integer(1));
+//		
+//		JMenu list = new JMenu();
+//		JButton test1 = new JButton(); test1.setText("ddd");
+//		list.add(test1);
+		
+		
+	}
+
+	private static void addTest(ArrayList<? extends Shape> array) {
+		
+	}
+	
+	private static ArrayList getTest() {
+		return null;
+	}
+	
+	
+
+}
